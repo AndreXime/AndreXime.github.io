4d0794d324327c8b25420a499293ae29a1b5fe4c
AndreXime <andreximenes536@gmail.com>
2023-12-23 20:28:06 -0300

    Tema Claro

--- scripts.js
+++ scripts.js
@@ -7,3 +7,17 @@ function Scroll(sectionId) {
       });
     }
 }
+
+document.getElementById('Dark').addEventListener('click', DarkTheme)
+document.getElementById('Light').addEventListener('click', LightTheme)
+
+function DarkTheme() {
+  document.documentElement.style.setProperty('--cor-Pallet1', '#282828');
+  document.documentElement.style.setProperty('--cor-Pallet2', '#191919');
+  document.documentElement.style.setProperty('--cor-Pallet3', '#ffffff');
+}
+function LightTheme() {
+  document.documentElement.style.setProperty('--cor-Pallet1', '#ffffff');
+  document.documentElement.style.setProperty('--cor-Pallet2', '#72A0C1');
+  document.documentElement.style.setProperty('--cor-Pallet3', '#000000');
+}
