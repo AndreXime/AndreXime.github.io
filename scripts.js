a064ba33e9297925090925e340e44466a1610152
AndreXime <andreximenes536@gmail.com>
2023-12-23 21:54:17 -0300

    sidebar

--- scripts.js
+++ scripts.js
@@ -8,16 +8,25 @@ function Scroll(sectionId) {
     }
 }
 
-document.getElementById('Dark').addEventListener('click', DarkTheme)
-document.getElementById('Light').addEventListener('click', LightTheme)
-
-function DarkTheme() {
-  document.documentElement.style.setProperty('--cor-Pallet1', '#282828');
-  document.documentElement.style.setProperty('--cor-Pallet2', '#191919');
-  document.documentElement.style.setProperty('--cor-Pallet3', '#ffffff');
+document.getElementById('Tema').addEventListener('click', Theme)
+let ThemeState = true
+function Theme() {
+  if(ThemeState){
+    document.documentElement.style.setProperty('--cor-Pallet1', '#282828');
+    document.documentElement.style.setProperty('--cor-Pallet2', '#191919');
+    document.documentElement.style.setProperty('--cor-Pallet3', '#ffffff');
+  }else{
+    document.documentElement.style.setProperty('--cor-Pallet1', '#ffffff');
+    document.documentElement.style.setProperty('--cor-Pallet2', '#72A0C1');
+    document.documentElement.style.setProperty('--cor-Pallet3', '#000000');
+  }
+  ThemeState = !ThemeState
 }
-function LightTheme() {
-  document.documentElement.style.setProperty('--cor-Pallet1', '#ffffff');
-  document.documentElement.style.setProperty('--cor-Pallet2', '#72A0C1');
-  document.documentElement.style.setProperty('--cor-Pallet3', '#000000');
+
+function expandirOuRecolherSidebar() {
+  var sidebar = document.getElementById("sidebar");
+  var larguraAtual = sidebar.offsetWidth;
+  var novaLargura = larguraAtual === 60 ? 200 : 60;
+
+  sidebar.style.width = novaLargura + "px";
 }
