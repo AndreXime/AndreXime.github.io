76c9ad55823c16b9f3b6fd46167b8575d8e39523
AndreXime <andreximenes536@gmail.com>
2023-12-24 00:42:06 -0300

    changes

--- scripts.js
+++ scripts.js
@@ -17,7 +17,7 @@ function Theme() {
     document.documentElement.style.setProperty('--cor-Pallet3', '#ffffff');
   }else{
     document.documentElement.style.setProperty('--cor-Pallet1', '#ffffff');
-    document.documentElement.style.setProperty('--cor-Pallet2', '#72A0C1');
+    document.documentElement.style.setProperty('--cor-Pallet2', '#191919');
     document.documentElement.style.setProperty('--cor-Pallet3', '#000000');
   }
   ThemeState = !ThemeState
